64ee3c7408e4a3d3a908b0267f0a64315c625594
WahajjKhalid <221422@students.au.edu.pk>
2025-10-01 12:05:42 +0500

    get details using appointment id

--- backend/src/controllers/Appointment/AppointmentController.js
+++ backend/src/controllers/Appointment/AppointmentController.js
@@ -2,6 +2,30 @@ const { statusCodes } = require("../../utils/statusCodesUtil");
 const { AppointmentService } = require("../../services/Appointment/AppointmentService");
 
 class AppointmentController {
+    async getAppointmentDetails(req, res) {
+        const { appointment_id } = req.params;
+        const { person_id } = req.user;
+
+        try {
+            const appointmentDetails = await AppointmentService.getAppointmentDetails(person_id, appointment_id);
+
+            res.status(statusCodes.OK).json({
+                data: appointmentDetails,
+                message: "Appointment details fetched successfully",
+                status: statusCodes.OK,
+                success: true
+            });
+        } catch (error) {
+            console.error(`Error in getAppointmentDetails: ${error.message}`);
+            res.status(error.status || statusCodes.INTERNAL_SERVER_ERROR).json({
+                data: null,
+                message: error.message || "error fetching appointment details",
+                status: error.status || statusCodes.INTERNAL_SERVER_ERROR,
+                success: false
+            });
+        }
+    }
+
     async getAppointmentsForPatient(req, res) {
         const { person_id } = req.user;
 
--- backend/src/routes/appointment/appointmentRoutes.js
+++ backend/src/routes/appointment/appointmentRoutes.js
@@ -4,6 +4,11 @@ const AppointmentController = require("../../controllers/Appointment/Appointment
 const router = express.Router();
 
 router.get(
+    "/:appointment_id",
+    AppointmentController.getAppointmentDetails
+)
+
+router.get(
     "/patient",
     AppointmentController.getAppointmentsForPatient
 );
