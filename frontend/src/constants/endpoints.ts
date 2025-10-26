e48ce90a48937b7df90b6b1a7b12a1f2e6b0fb73
Junaid Ahmed <dev.eraz3r@gmail.com>
2025-10-26 19:02:27 +0500

    new backend routes

--- frontend/src/constants/endpoints.ts
+++ frontend/src/constants/endpoints.ts
@@ -2,145 +2,122 @@ const EndPoints = {
   auth: {
     signIn: "/auth/sign-in", //POST
     signUp: "/auth/sign-up", //POST
-    refreshToken: "/auth/refresh-jwt", //POST
-    google: "/auth/google", //GET
-    // googleCallback: "/auth/google/callback", //GET (handled by backend)
-    forgotPassword: "/token/password-reset", //POST
-    resetPassword: "/token/password-reset/reset", //POST
+    refreshJWT: "/auth/refresh-jwt", //POST
+    sendOrResendEmailVerificationToken: "/token/send-email-verification", //POST
+    sendOrResendEmailPasswordResetToken: "/token/send-password-reset", //POST
+    verifyEmail: "/token/verify-email", //POST
+    passwordReset: "/token/reset-password", //POST
   },
-  emailVerification: {
-    verify: "/token/email-verification/verify", //POST
-    sendOrResend: "/token/email-verification/", //POST
+  appointment: {
+    getAllPatient: "/appointment/patient", // GETa
+    getAllDoctor: "/appointment/doctor", // GET
+    getAllHospital: "/appointment/hospital", // GET
+    insert: "/appointment", // POST
+    approve: "/appointment/approve/:appointment_id", // PUT
+    deny: "/appointment/deny/:appointment_id", // PUT
+    cancelByPatient: "/appointment/cancel/:appointment_id", // PUT (for patient)
+    rescheduleByPatient: "/appointment/patient-reschedule/:appointment_id", // PUT
+    rescheduleByHospital: "/appointment/hospital-reschedule/:appointment_id", // PUT
+    startByDoctor: "/appointment/start/:appointment_id", // PUT
+    setLabTestRequiredByDoctor: "/appointment/require-lab-test/:appointment_id", // PUT
+    setLabPrescriptionRequiredByDoctor: "/appointment/require-prescription/:appointment_id", // PUT
+    completeByDoctor: "/appointment/complete-doctor/:appointment_id", // PUT
+    completeLabTestByLabTechnician: "/appointment/complete-lab-test/:appointment_id", // PUT
+    completePrescriptionByLabTechnician: "/appointment/complete-prescription/:appointment_id", // PUT
+  },
+  doctor: {
+    get: "/doctor", // GET
+    getDoctorsForAppointmentBooking: "/doctor/appointment-booking", // GET
+    update: "/doctor",  // PUT
+    updateStatus: "/doctor/status/:doctor_id",  // PUT
+    updateHospitalByDoctor: "/doctor/update-hospital",  // PUT
+    updateHospitalByHospitalAdmin: "/doctor/remove-hospital/:doctor_id",  // PUT
+    getHospitalassociatedDoctors: "/doctor/hospital", // GET
+  },
+  ehrAccess: {
+    getForPatient: "/ehr-access/patient", //GET
+    getForDoctor: "/ehr-access/doctor", //GET
+    requestByDoctor: "/ehr-access/request", //POST
+    denyByPatient: "/ehr-access/deny/:ehr_access_request_id", //PUT
+    grantByPatient: "/ehr-access/grant/:ehr_access_request_id", //PUT
+    revokeByPatient: "/ehr-access/revoke/:ehr_access_request_id", //PUT
+  },
+  hospital: {
+    get: "/hospital", // GET
+    insert: "/hospital", // POST
+    update: "/hospital/:hospital_id", // PUT
+  },
+  hospitalAssociationRequest: {
+    getByPerson: "/hospital-association-request/hospital-staff", // GET
+    getByHospital: "/hospital-association-request/hospital-staff", // GET TODO: verify this endpoint
+    insert: "/hospital-association-request", // POST
+    approve: "/hospital-association-request/accept/:hospital_association_request_id", // POST
+    deleteByStaff: "/hospital-association-request/hospital-staff/:hospital_association_request_id", // DELETE
+    deleteByPerson: "/hospital-association-request/person/:hospital_association_request_id", // DELETE
+    deleteAllByPerson: "/hospital-association-request/person", // DELETE
+  },
+  hospitalPannelList: {
+    getAll: "/hospital-panel-list", // GET
+    insert: "/hospital-panel-list", // POST
+    delete: "/hospital-panel-list/:hospital_pannel_list_id", // DELETE
+  },
+  hospitalStaff: {
+    getAdminsForSuperAdmin: "/hospital-staff/admin",  // GET
+    insert: "/hospital-staff", // POST
+    delete: "/hospital-staff/:hospital_staff_id", // DELETE
+    get: "/hospital-staff", // GET
+    getAll: "/hospital-staff/all",  // GET
+
+  },
+  insuranceCompany: {
+    get: "/insurance-company", // GET
+    insert: "/insurance-company", // POST
+    update: "/insurance-company/:insurance_company_id", // PUT
+  },
+  labTest: {
+    get: "/lab-test", // GET
+    insert: "/lab-test", // POST
+    update: "/lab-test/:lab_test_id", // PUT
+  },
+  logs: {
+    getAll: "/log", // GET
+  },
+  medicine: {
+    getAll: "/medicine", // GET
+    insert: "/medicine", // POST
   },
   notification: {
-    list: "/notification", // GET
-    markAsRead: "/notification/:notification_id", // PUT
-    markAllAsRead: "/notification", // PUT
+    getAll: "/notification", // GET
+    update: "/notification/:notification_id", // PUT
+    updateAll: "/notification", // PUT
     delete: "/notification/:notification_id", // DELETE
     deleteAll: "/notification", // DELETE
+  },
+  patient: {
+    get: "/patient/", // GET
+    update: "/patient/", // PUT
+  },
+  patientInsurance: {
+    get: "/patient-insurance",  // GET
+    insert: "/patient-insurance",  // POST
+    sendVerificationRequest: "/patient-insurance/verify/:patient_insurance_id",  // POST
+    update: "/patient-insurance/:patient_insurance_id",  // PUT
+    delete: "/patient-insurance/:patient_insurance_id",  // DELETE
   },
   person: {
     get: "/person", // GET
     update: "/person", // PUT
-  },
-  patient: {
-    // dashboard: "/patient/dashboard",
-    profile: "/patient/profile",
-  },
-  doctor: {
-    dashboard: "/doctor/dashboard",
-    profile: "/doctor/", //GET
-    getForHospital: "/doctor/hospital/", //GET (:hospital_id)
-    getAll: "/doctor/getAll", //GET
-  },
-  hospital: {
-    create: "/hospital/", //POST (name, address)
-    get: "/hospital/", //GET
-    update: "/hospital/", //PUT (:hospital_id) (name, address)
-    delete: "/hospital/", //DELETE (:hospital_id)
-  },
-  hospitalStaff: {
-    get: "/hospital/staff", //GET ({persion_id})
-    // /super-admin/all-hospital-admins
-    getAll: "/hospital/staff", // GET (:hospital_id)
-    insert: "/hospital/staff", //POST  ({email, hospital_id, role})
-    delete: "/hospital/staff", //DELETE (:hospital_staff_id)
-  },
-  admin: {
-    dashboard: "/admin/dashboard",
-    users: "/admin/users",
-    allHospitals: "/hospital/staff/super-admin/all-hospital-admins",
-  },
-  appointments: {
-    uploadNote: "/doctor/note/", //POST ({appointment_id, note})
-    getNote: "/doctor/note/", //GET (:appointment_id)
-    updateStatus: "/appointment/", //PUT (:appointment_id)
-    patient: "/appointment/patient", //GET
-    doctor: "/appointment/doctor", //GET
-    hospital: "/appointment/hospital", //GET
-    get: "/appointment/appointment-details/", //GET (:appointment_id)
-    update: "/appointment/", //PUT (:appointment_id)
-    request: {
-      insert: "/appointment/request/", //POST
-      patient: "/appointment/request/patient", //GET
-      hospital: "/appointment/request/hospital", //GET
-      updateHospitalStatus: "/appointment/request/hospital", //PUT (:appointment_request_id)
-      reschedule: "/appointment/request/reschedule/", //PUT (:appointment_request_id)
-      cancel: "/appointment/request/cancel/", //PUT (:appointment_request_id)
-    },
-  },
-  hospitalAssociationRequest: {
-    person: "/hospital/association-request/person", // GET - Get requests for logged-in person
-    hospital: "/hospital/association-request/hospital/", // GET (:hospital_id) - Get requests for hospital
-    create: "/hospital/association-request/", // POST - Create new request
-    approve: "/hospital/association-request/approve/", // PUT (:hospital_association_request_id)
-    delete: "/hospital/association-request/", // DELETE (:hospital_association_request_id)
-    deleteByPersonAndRole: "/hospital/association-request/person", // DELETE - Delete by person and role
-  },
-  logs: "/log", //GET
-  documents: {
-    get: "/document", //GET
-    upload: "/document/upload", //POST
-  },
-  systemAdmin: {
-    getAll: "/system/admin", // GET
-    create: "/system/admin", // POST
-    delete: "/system/admin", // DELETE /:system_admin_id
-  },
-  // ehrAccess: {
-  //   patient: "/ehr/access/patient/requests", //GET
-  //   doctor: "/ehr/access/doctor/requests", //GET
-  //   create: "/ehr/access", //POST
-  //   updateStatus: "/ehr/access/status", //PUT (:ehr_access_request_id)
-  //   grant: "/ehr/access/grant", //PUT (:ehr_access_request_id)
-  //   revoke: "/ehr/access/revoke", //PUT (:ehr_access_request_id)
-  // },
-  ehrAccessRequest: {
-    // get: "/ehr/access-request", //GET
-    patient: "/ehr/access-request/patient/requests", //GET
-    doctor: "/ehr/access-request/doctor/requests", //GET
-    create: "/ehr/access-request", //POST
-    approve: "/ehr/access-request/approve", //PUT (:ehr_access_request_id)
-    revoke: "/ehr/access-request/revoke", //PUT (:ehr_access_request_id)
-  },
-  ehr: {
-    appointment: "/ehr/appointment", //POST
-    doctor: "/ehr/doctor", //POST
-  },
-  insuranceCompany: {
-    getAll: "/insurance/company", // GET
-    create: "/insurance/company", // POST
-    update: "/insurance/company/:insurance_company_id", // PUT
-    delete: "/insurance/company/:insurance_company_id", // DELETE
-  },
-  patientInsurance: {
-    getAll: "/patient/insurance", // GET
-    create: "/patient/insurance", // POST
-    update: "/patient/insurance/:patient_insurance_id", // PUT
-    delete: "/patient/insurance/:patient_insurance_id", // DELETE
-  },
-  hospitalPannel: {
-    getAll: "/hospital/pannel", // GET
-    insert: "/hospital/pannel", // POST
-    remove: "/hospital/pannel/:hospital_pannel_id", // DELETE
-  },
-  labTest: {
-    getAll: "/lab-test", //GET
-    create: "/lab-test", //POST
-    update: "/lab-test/:lab_test_id", //PUT
-  },
-  medicine: {
-    getAll: "/medicine", //GET
-    create: "/medicine", //POST
-    update: "/medicine/:medicine_id", //PUT
+    delete: "/person", // DELETE
   },
   prescription: {
-    getAll: "/prescription", //GET TODO: add this endpoint in backend
-    getByAppointment: "/prescription/:appointment_id", //GET
-    create: "/prescription", //POST
-    update: "/prescription/:prescription_id", //PUT
-    delete: "/prescription/:prescription_id", //DELETE
+    insert: "/prescription", // POST
+    getAgainstAppointment: "/prescription", // GET
   },
+  systemAdmin: {
+    getAll: "/system-admin", // GET
+    insert: "/system-admin", // POST
+    delete: "/system-admin/:system_admin_id", // DELETE
+  }
 } as const;
 
 export default EndPoints;
